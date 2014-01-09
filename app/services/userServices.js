75d18f445b5daf10a29a40437c6077b9535a115b
William Oliveira <sudowilliam@gmail.com>
2014-01-09 03:15:32 -0200

    users mock

--- app/services/userServices.js
+++ app/services/userServices.js
@@ -1,3 +1,30 @@
+var users = [
+  { 
+    picture: 'http://placehold.it/90x90', 
+    name: 'William Oliveira', 
+    ocupation: 'Freelance Web Developer',
+    email: 'sudowilliam@gmail.com'
+   },
+   { 
+    picture: 'http://placehold.it/90x90', 
+    name: 'Darth Vader', 
+    ocupation: 'Sith Lord',
+    email: 'vader@gmail.com'
+   },
+   { 
+    picture: 'http://placehold.it/90x90', 
+    name: 'Luke Skywalker', 
+    ocupation: 'Jedi Knight',
+    email: 'sudowilliam@gmail.com'
+   },
+   { 
+    picture: 'http://placehold.it/90x90', 
+    name: 'Han Solo', 
+    ocupation: 'Pilot',
+    email: 'solo@gmail.com'
+   }
+];
+
 App.factory('User', [
   function() {
     return {
